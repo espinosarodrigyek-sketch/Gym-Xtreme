9e10baa96db40a6c96658d1dd8aeed28201b19f5
espinosarodrigyek-sketch <espinosarodrigyek@gmail.com>
2026-06-12 22:34:44 -0500

    Update settings.py

--- gym/settings.py
+++ gym/settings.py
@@ -95,14 +95,11 @@ WSGI_APPLICATION = 'gym.wsgi.application'
 # BASE DE DATOS (RENDER POSTGRESQL)
 # =========================
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT', '3306'),
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
