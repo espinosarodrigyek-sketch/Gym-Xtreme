04f9a42ebfd88e77c1d4b4e28ecfacd3731963c1
espinosarodrigyek-sketch <espinosarodrigyek@gmail.com>
2026-06-13 17:59:15 -0500

    Update settings.py

--- gym/settings.py
+++ gym/settings.py
@@ -99,13 +99,23 @@ WSGI_APPLICATION = 'gym.wsgi.application'
 # BASE DE DATOS (RENDER POSTGRESQL)
 # =========================
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600,
-        ssl_require=True
-    )
-}
+import os
+
+if os.environ.get('DATABASE_URL'):
+    import dj_database_url
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=os.environ.get('DATABASE_URL'),
+            conn_max_age=600
+        )
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # =========================
